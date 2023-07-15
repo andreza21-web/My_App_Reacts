f2b0e6bb62f317f6526a718aca2f9a23e84b5e75
Andreza Pessoa <andreza.21pessoa@gmail.com>
2023-07-15 19:15:44 -0300

    first project

---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe("test suit", () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+})
+
+
