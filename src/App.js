cfeb18fe778f32f79e347343b7a6adb4a50b47a7
Andreza Pessoa <andreza.21pessoa@gmail.com>
2023-07-15 20:22:04 -0300

    primeira atividade do curso

--- src/App.js
+++ src/App.js
@@ -7,16 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Curso de ReactJs
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='desafio'>Desafio 01</h1>
+        
       </header>
     </div>
   );
